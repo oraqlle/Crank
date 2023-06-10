e6c482bb25a38d40143d834c8fb3cf50eee0b049
oraqlle <41113853+oraqlle@users.noreply.github.com>
2023-06-10 18:21:57 +1000

    Swapped to Meson build system. Removed bpt build system. Restructure example project to use Meson.

--- include/crank/crank.hxx
+++ include/crank/crank.hxx
@@ -1,9 +1,9 @@
 // -*- C++ -*- Header compatibility <crank.hxx>
 
-/// \brief 
+/// \brief
 ///
 /// Author: Tyler Swann (tyler.swann05@gmail.com)
-/// 
+///
 /// Header Version: v0.1.0
 ///
 /// Date: 02-01-2023
@@ -14,10 +14,10 @@
 /// \file crank.hxx
 
 #ifndef CRANK_
-#   define CRANK_
+#define CRANK_
 
 #include <engine/engine.hxx>
-#include <states/state_interface.hxx>
 #include <states/id.hxx>
+#include <states/state_interface.hxx>
 
-#endif  /// CRANK_
+#endif /// CRANK_
